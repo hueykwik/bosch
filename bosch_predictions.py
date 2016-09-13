72cf10f5f9a2033e01c8d320c780106169244fe9
Huey Kwik <huey.kwik@gmail.com>
2016-09-13 13:49:11 -0700

    Uses sklearn Pipeline to impute, classify, and predict.

--- bosch_predictions.py
+++ bosch_predictions.py
@@ -9,16 +9,14 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+from sklearn.pipeline import Pipeline
+from sklearn.base import TransformerMixin
+
 import time
 
 from sklearn.linear_model import LogisticRegression
 
 matplotlib.style.use('ggplot')
-# np.set_printoptions(threshold=np.inf)
-
-# Example plot
-#plt.plot(range(10), 'o')
-#plt.show()
 
 # Load training data
 #categorical = pd.read_csv('train_categorical.csv', nrows=100)
@@ -33,47 +31,48 @@ success = numerics.loc[numerics.Response == 0].sample(53, random_state=9).index
 
 indices = failed.append(success)
 
-# combine them into a dataframe
-
 # Load test data
 
 start = time.time()
 numerics_test = pd.read_csv('test_numeric.csv')
 print(time.time() - start)
 
-# Run some ML to get predictions
-
 # Predict on numerics
 y_m = numerics.loc[indices, 'Response']
 X_m = numerics[numerics.columns.difference(['Id', 'Response'])]
 X_m = X_m.loc[indices]
 
-# Impute NaNs to 0
-for col in list(X_m):
-    X_m[col].loc[X_m[col].isnull()] = 0
-    
-print(time.time() - start)
-    
-#X_m['L3_S48_F4196'].sum()  # currently 0
-#numerics['L3_S48_F4196'].sum()  # 8.920000000000007
-
-#numerics['L3_S48_F4196'].shape
-
-clf = LogisticRegression()
-clf.fit(X_m, y_m)
-
-print(time.time() - start)
-
-#print(clf.coef_)
-
 X_m_test = numerics_test[numerics_test.columns.difference(['Id', 'Response'])]
-# Impute NaNs to 0
-for col in list(X_m_test):
-    X_m_test[col].loc[X_m_test[col].isnull()] = 0
 
-y_pred = clf.predict(X_m_test)
+X_m.head()
+X_m.loc[:, 'L3_S51_F4256']  # example of NaN
+
+class ZeroImputer(TransformerMixin):
+    def __init__(self):
+        """Impute missing values to zero.
+        """
+        
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        return X.fillna(0)
+  
+# Test imputer turns NAs to 0      
+# ZeroImputer().fit_transform(X_m).loc[,'L3_S51_F4256']
+
+# Pipeline example
+pipe_clf = Pipeline([('impute', ZeroImputer()),
+                     ('clf', LogisticRegression()),
+])
+
+
+pipe_clf = pipe_clf.fit(X_m, y_m)
+y_pred = pipe_clf.predict(X_m_test)
 print(y_pred.sum())
 
+print(time.time() - start)
+  
 # Submission
 submission = pd.read_csv('sample_submission.csv')
 submission.Response = y_pred
