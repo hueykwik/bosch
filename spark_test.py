2e180836acf0a8e9f01da8073cdf0f6a2b436d95
Huey Kwik <huey.kwik@gmail.com>
2016-09-22 10:32:30 -0700

    Fixes the random seed for reproducibility.

--- spark_test.py
+++ spark_test.py
@@ -18,63 +18,65 @@ from pyspark.ml.classification import GBTClassifier
 import numpy as np
 import pandas as pd
 
-spark = SparkSession\
-    .builder\
-    .appName("example-spark")\
-    .config("spark.sql.crossJoin.enabled","true")\
-    .getOrCreate()
-#sc = SparkContext()
-#sqlContext = SQLContext(sc)
+if __name__ == "__main__":
+    spark = SparkSession\
+        .builder\
+        .appName("example-spark")\
+        .config("spark.sql.crossJoin.enabled","true")\
+        .getOrCreate()
+    #sc = SparkContext()
+    #sqlContext = SQLContext(sc)
 
-print('reading csv')
-train = spark.read.csv("data/train_numeric.csv", header="true", inferSchema="true")
-print('done reading csv')
-print('filling na')
-train = train.na.fill(0.0)
-print('done filling na')
+    print('reading csv')
+    train = spark.read.csv("data/train_numeric.csv", header="true", inferSchema="true")
+    print('done reading csv')
+    print('filling na')
+    train = train.na.fill(0.0)
+    print('done filling na')
 
-from pyspark.ml.feature import VectorAssembler
-ignore = ['Id', 'Response']
-lista=[x for x in train.columns if x not in ignore]
+    from pyspark.ml.feature import VectorAssembler
+    ignore = ['Id', 'Response']
+    lista=[x for x in train.columns if x not in ignore]
 
-assembler = VectorAssembler(
-    inputCols=lista,
-    outputCol='features')
+    assembler = VectorAssembler(
+        inputCols=lista,
+        outputCol='features')
 
-train = (assembler.transform(train).select('Response',"features"))
+    train = (assembler.transform(train).select('Response',"features"))
 
-## Split the data into training and test sets (30% held out for testing)
-(trainingData, testData) = train.randomSplit([0.7, 0.3])
-# (trainingData, testData) = train.randomSplit([0.001, 0.99])  # Just for speed while testing correctness
+    ## Split the data into training and test sets (30% held out for testing)
+    (trainingData, testData) = train.randomSplit([0.7, 0.3], seed=24)
+    # (trainingData, testData) = train.randomSplit([0.001, 0.99])  # Just for speed while testing correctness
 
-# Train a GBT model.
-gbt = GBTClassifier(labelCol="Response", featuresCol="features", maxIter=10)
-#lr = LogisticRegression(labelCol="Response", featuresCol="features", maxIter=10, regParam=0.3, elasticNetParam=0.8)
+    # Train a GBT model.
+    gbt = GBTClassifier(labelCol="Response", featuresCol="features", maxIter=1, seed = 24)
 
-# Chain indexers and GBT in a Pipeline
-pipeline = Pipeline(stages=[gbt])
+    # Chain indexers and GBT in a Pipeline
+    pipeline = Pipeline(stages=[gbt])
 
-print('training model')
-# Train model.  This also runs the indexers.
-model = pipeline.fit(trainingData) # Make predictions.
+    print('training model')
+    # Train model.  This also runs the indexers.
+    model = pipeline.fit(trainingData) # Make predictions.
 
-# Test against validation set
-print('testing against validation set')
-predictions = model.transform(testData)
-predsGBT=predictions.select("prediction").rdd.map(lambda r: r[0]).collect()
-preds=np.asarray(predsGBT).astype(int)
+    # Test against validation set
+    print('testing against validation set')
+    predictions = model.transform(testData)
+    predsGBT=predictions.select("prediction").rdd.map(lambda r: r[0]).collect()
+    preds=np.asarray(predsGBT).astype(int)
 
-# Make predictions and submit
-data_test = spark.read.csv("data/test_numeric.csv", header="true", inferSchema="true")
-data_test = data_test.na.fill(0.0)
-data_test = (assembler.transform(data_test).select("features"))
+    # Make predictions and submit
+    data_test = spark.read.csv("data/test_numeric.csv", header="true", inferSchema="true")
+    data_test = data_test.na.fill(0.0)
+    data_test = (assembler.transform(data_test).select("features"))
 
-preds = model.transform(data_test)
+    preds = model.transform(data_test)
 
-predsGBT=preds.select("prediction").rdd.map(lambda r: r[0]).collect()
+    predsGBT=preds.select("prediction").rdd.map(lambda r: r[0]).collect()
 
-sub = pd.read_csv("data/sample_submission.csv")
-sub['Response'] = np.asarray(predsGBT).astype(int)
-sub.to_csv('bosch-spark.csv', index=False)
+    sub = pd.read_csv("data/sample_submission.csv")
+    sub['Response'] = np.asarray(predsGBT).astype(int)
+    sub.to_csv('bosch-spark.csv', index=False)
 
-print('done')
+    print('done')
+
+    spark.stop()
